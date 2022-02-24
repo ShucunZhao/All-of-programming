23acb32b8b5ea795d645439b6ba940300b0be33e
ShucunZhao <frankabe001@gmail.com>
2022-02-24 11:20:57 +0800

    Q4.7

---
+++ Chapters/Chapter4/Q4.7.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int printFactors(int n){
+    if(n>1){
+	if((n%2)=0){
+	    
+	}
+    }
+}
+
+int main(){
+    int n;
+    printf("Please enter a integer:\n");
+    scanf("%d", &n);
+
+    return 0;
+}
