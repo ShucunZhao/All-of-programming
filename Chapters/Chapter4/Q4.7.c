bbdfef964d81ce57c945e135b34e5f52730656e4
ShucunZhao <frankabe001@gmail.com>
2022-02-24 12:37:03 +0800

    submited Q4.7

--- Chapters/Chapter4/Q4.7.c
+++ Chapters/Chapter4/Q4.7.c
@@ -1,10 +1,19 @@
 #include<stdio.h>
 
-int printFactors(int n){
+void printFactors(int n){//This problem is up to if u can use the right type of loop 'for-loop'. 
     if(n>1){
-	if((n%2)=0){
-	    
+	for(int i=2; i<=n; i++){
+	    while(n%i==0){
+		printf("%d", i);
+		n/=i;
+		if(n!=1){
+		    printf(" * ");
+		}
+	    }
 	}
+    }
+    else{
+    printf("Warning: The minimum prime number is 2, please enter an integer larger than 1.");
     }
 }
 
@@ -12,6 +21,6 @@ int main(){
     int n;
     printf("Please enter a integer:\n");
     scanf("%d", &n);
-
+    printFactors(n);
     return 0;
 }
