0e6028e7a82500cdd89495a67e61a1c0deae8c7b
ShucunZhao <frankabe001@gmail.com>
2022-03-22 20:30:24 +0800

    submitted Q10.2.

---
+++ Chapters/Chapter10/Q10.2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int myatoi(const char * str){
+	_Bool flag;
+	while(*str==' '){
+		str++;
+	}
+	if(*str=='+'){
+		flag = 1;
+		str++;
+	}
+	if(*str=='-'){
+		flag = 0;
+		str++;
+	}
+	if(*str<'0'||*str>'9'||*str=='\0'){
+		return 0;
+	}
+	int i = 0;
+	while(*str>='0'&&*str<='9'&&*str!='\0'){
+		i=i*10+*str-'0';
+		str++;
+	}
+	if(flag){
+		printf("+");
+	}
+	if(!flag){
+		printf("-");
+	}
+	return i;
+}
+
+int main(){
+	const char str[]="  -345xcr";
+	printf("The result of convertion is:\n");
+	printf("%d\n", myatoi(str));
+	return 0;
+}
