c892203ef5aee73aee04fe801596c679e1655e87
ShucunZhao <frankabe001@gmail.com>
2022-04-02 13:44:25 +0800

    submitted Q11.3.

---
+++ Chapters/Chapter11/Q11.3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#define LINE_SIZE 512
+
+int main(int argc, char ** argv){
+	if(argc>1){
+		for(int i=1;i<argc;i++){
+			FILE * f = fopen(argv[i], "r");
+			if(f==NULL){
+				printf("Not %s file name exit.\n", argv[i]);
+				continue;
+			}
+			else{
+				int c;
+				while((c=fgetc(f))!=EOF){
+					printf("%c",c);
+				}
+			}
+			fclose(f);
+		}
+	}
+	else{
+		char line[LINE_SIZE];
+		while(fgets(line,LINE_SIZE,stdin)!=NULL){
+			printf("%s", line);
+			//if(fputs(line,stdout)<0){
+			//	perror("fputs failed\n");
+			//}
+			//fputs(line,stdout);
+		}
+	}
+	return 0;
+}
