8bee12a5ea38e5b50ac40d49f5cc96bbe9baba90
ShucunZhao <frankabe001@gmail.com>
2022-04-10 10:12:18 +0800

    updated encrypt all to write output file.

--- Homeworks/046_read_encr/encrypt_all.c
+++ Homeworks/046_read_encr/encrypt_all.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-void encrypt(FILE * f, int key){
+void encrypt(FILE * f1, int key, FILE * f2){
   int c;
-  while ((c = fgetc(f)) != EOF) {
+  int i=0;
+  //char LINE[1024]={'\0'}
+  while ((c = fgetc(f1)) != EOF) {
     if (isalpha(c)) {
       c = tolower(c);
       c -= 'a';
@@ -12,27 +14,37 @@ void encrypt(FILE * f, int key){
       c %= 26;//26
       c += 'a';//25+97=122
     }
-    printf("%c", c);
+    //printf("%c", c);
+	//LINE[i] = c;
+	fprintf(f2, "%c", c);
+	i++;
   }
 }
 
 int main(int argc, char ** argv) {
-  if (argc != 3) {
+  if (argc != 4) {
     fprintf(stderr,"Usage: encrypt key inputFileName\n");
     return EXIT_FAILURE;
   }
+  FILE * f1 = fopen(argv[2], "r");
+  FILE * f2 = fopen(argv[3], "w+");
+  if (f1 == NULL) {
+    perror("Could not open file1.\n");
+    return EXIT_FAILURE;
+  }
+  if (f2 == NULL) {
+    perror("Could not open file2.\n");
+    return EXIT_FAILURE;
+  }
+ 
   int key = atoi(argv[1]);
   if (key == 0) {
     fprintf(stderr,"Invalid key (%s): must be a non-zero integer\n", argv[1]);
     return EXIT_FAILURE;
   }
-  FILE * f = fopen(argv[2], "r");
-  if (f == NULL) {
-    perror("Could not open file");
-    return EXIT_FAILURE;
-  }
-  encrypt(f,key);
-  if (fclose(f) != 0) {
+ 
+  encrypt(f1,key,f2);
+  if (fclose(f1) != 0||fclose(f2) != 0) {
     perror("Failed to close the input file!");
     return EXIT_FAILURE;
   }
