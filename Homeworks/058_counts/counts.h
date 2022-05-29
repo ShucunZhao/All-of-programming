ee05a204f5393212c6ea62c74cddb4ad5a280067
ShucunZhao <frankabe001@gmail.com>
2022-05-29 17:47:06 +0800

    submitted hw058.

---
+++ Homeworks/058_counts/counts.h
@@ -0,0 +1,23 @@
+#ifndef __COUNTS_H__
+#define __COUNTS_H__
+
+typedef struct countInfo_tag{
+	char * value;
+	size_t times;
+}countInfo_t;
+
+typedef struct counts_tag{
+	size_t nums;
+	countInfo_t ** countInfo;
+	countInfo_t * unknow;
+}counts_t;
+
+counts_t * createCounts(void);
+
+void addCount(counts_t * counts, const char * name);
+
+void printCounts(counts_t * counts, FILE * f);
+
+void freeCounts(counts_t * counts);
+
+#endif
