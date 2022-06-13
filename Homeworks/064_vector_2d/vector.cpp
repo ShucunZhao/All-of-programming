e98889a7ea54ac2111bfc8e31924d6997d928f05
ShucunZhao <frankabe001@gmail.com>
2022-06-13 19:16:11 +0800

    submitted hw064.

---
+++ Homeworks/064_vector_2d/vector.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include "vector.hpp"
+using namespace std;
+
+void Vector2D::initVector(double init_x, double init_y){
+	x = init_x;
+	y = init_y;
+}
+
+double Vector2D::getMagnitude() const{
+	return sqrt(pow(x,2)+pow(y,2));
+}
+
+Vector2D Vector2D::operator+(const Vector2D & rhs) const{
+	Vector2D ans;
+	ans.x = this->x + rhs.x;
+	ans.y = this->y + rhs.y;
+	return ans;
+}
+
+Vector2D & Vector2D::operator+=(const Vector2D & rhs){
+	this->x = x+rhs.x;//U can use x directly here, no need to use this->
+	this->y = y+rhs.y;
+	return *this;
+}
+
+double Vector2D::dot(const Vector2D & rhs) const{
+	return this->x*rhs.x + this->y*rhs.y;
+}
+
+void Vector2D::print() const{
+	printf("<%.2f, %.2f>", x, y);
+}
